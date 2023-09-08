8088eba74818450ddd7f1e02aecf42c5f2eee8d6
quentin2417 <quintlodu62@orange.fr>
2023-09-08 20:19:21 +0200

    base

---
+++ inc/haut.inc.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mangas-Zone</title>
+    <link rel="stylesheet" href="<?php echo RACINE_SITE ?>inc/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="<?php echo RACINE_SITE ?>inc/js/main.js"></script>
+</head>
+<body>
+    <header>
+        <nav class="topnav" id="mytopnav">
+            <a href="<?php echo RACINE_SITE; ?>">LOGO</a>
+            <a href="<?php echo RACINE_SITE; ?>recherche.php">barre de recherche</a>
+            <a href="<?php echo RACINE_SITE; ?>inscription.php"> Inscription</a>
+            <a href="<?php echo RACINE_SITE; ?>commande.php">Commandes</a>
+            <a href="<?php echo RACINE_SITE; ?>panier.php">Panier</a>
+            <a href="javascript:void(0);" class="icon" ondblclick="togglenav()">
+            <i class="fa fa-bars"></i>
+            </a>
+        </nav>
+        <racnav class="racnav" id="myracnav">
+            <a href="<?php echo RACINE_SITE; ?>">One Piece</a>
+            <a href="<?php echo RACINE_SITE; ?>">Dragon Ball</a>
+            <a href="<?php echo RACINE_SITE; ?>">Naruto</a>
+            <a href="<?php echo RACINE_SITE; ?>">Demon Slayer</a>
+            <a href="<?php echo RACINE_SITE; ?>">Hunter X Hunter</a>
+            <a href="<?php echo RACINE_SITE; ?>">Attaque des Titans</a>
+            <a href="<?php echo RACINE_SITE; ?>">Bleach</a>
+            <a href="<?php echo RACINE_SITE; ?>">Jujutsu Kaisen</a>
+            <!-- <a href="javascript:void(0);" class="icon" ondblclick="togglenav()">
+            <i class="fa fa-flash"></i> -->
+        </racnav>
+    </header>
+    <section>
+        <div class="conteneur">
