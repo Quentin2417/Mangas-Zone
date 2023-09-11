8dabfb97f5064817f315e0e3ad06855241d078f9
quentin2417 <quintlodu62@orange.fr>
2023-09-11 08:51:19 +0200

    chemin

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ require('./inc/init.inc.php');
 require('./inc/haut.inc.php');
 ?>
 <div class="couv" id="mycouv">
-    <a href="/"><img src="./inc/img/couverture/couv_one_piece.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_dbz.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_naruto.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_demon_slayer.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_HxH.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_snk.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_bleach.png" alt=""></a>
-    <a href="/"><img src="./inc/img/couverture/couv_jujustu_kaisen.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_one_piece.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_dbz.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_naruto.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_demon_slayer.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_HxH.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_snk.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_bleach.png" alt=""></a>
+    <a href="/"><img src="./inc/img/couverture/couv_accueil/couv_jujustu_kaisen.png" alt=""></a>
 </div>
 <?php
 require('./inc/bas.inc.php');
