98a140b64f8872b7ac5b3afd4e47ba1863345510
quentin2417 <quintlodu62@orange.fr>
2023-09-20 15:49:35 +0200

    cookie

--- inc/bas.inc.php
+++ inc/bas.inc.php
@@ -2,10 +2,10 @@
 </section>
 <footer>
     <div class="bas">
-        <a href="<?php RACINE_SITE; ?>conditions_d_utilisation.php">Conditions d'utilisation</a>
+        <a href="<?php RACINE_SITE; ?>conditions.php">Conditions d'utilisation</a>
         <a href="<?php RACINE_SITE; ?>aide.php">Aides</a>
         <a href="<?php RACINE_SITE; ?>cookies.php">Cookies</a>
-        <a href="<?php RACINE_SITE; ?>contactez_nous.php">Contactez-nous</a>
+        <a href="<?php RACINE_SITE; ?>contact.php">Contactez-nous</a>
     </div>
     <?php echo date ('Y'); ?> -@2000-2023, Mangas_Zone.com ou ses affiliés.
 </footer>
